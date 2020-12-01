420df97b835dd9e12c465241ffaf7de5cc475d2e
주이식 <helios2sic@gmail.com>
2020-12-01 16:10:01 +0900

    [Feature] 앱에서 토큰값에 해당하는 기기에 알림전송 기능추가

---
+++ IF_2020/app/src/main/java/org/sejonguniv/if_2020/model/UserData.java
@@ -0,0 +1,5 @@
+package org.sejonguniv.if_2020.model;
+
+public class UserData {
+    public String FCMToken;
+}
--- IF_2020/app/src/main/java/org/sejonguniv/if_2020/ui/user/home/HomeFragment.java
+++ IF_2020/app/src/main/java/org/sejonguniv/if_2020/ui/user/home/HomeFragment.java
@@ -8,15 +8,23 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.FragmentTransaction;
 
+import com.google.firebase.iid.FirebaseInstanceId;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.sejonguniv.if_2020.R;
 import org.sejonguniv.if_2020.base.BaseFragment;
 import org.sejonguniv.if_2020.databinding.FragmentHomeBinding;
+import org.sejonguniv.if_2020.model.UserData;
 import org.sejonguniv.if_2020.ui.admin.home.AdminHomeFragment;
 import org.sejonguniv.if_2020.ui.admin.notice.AdminNoticeFragment;
 import org.sejonguniv.if_2020.ui.user.list.ListFragment;
 
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Calendar;
 
 
@@ -35,11 +43,43 @@ public class HomeFragment extends BaseFragment<FragmentHomeBinding, HomeViewMode
         binding.checkButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
-                AdminNoticeFragment homeFragment = new AdminNoticeFragment();
-                ListFragment listFragment = new ListFragment();
+                UserData userData = new UserData();
+                userData.FCMToken = FirebaseInstanceId.getInstance().getToken();
+                JSONObject root = new JSONObject();
+                JSONObject notification = new JSONObject();
 
-                transaction.replace(R.id.frame_layout, listFragment).commitAllowingStateLoss();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            notification.put("body", "1201");
+                            notification.put("title", "1201 test");
+                            root.put("notification", notification);
+                            root.put("to", userData.FCMToken);
+                            URL url = new URL("https://fcm.googleapis.com/fcm/send");
+                            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                            conn.setRequestMethod("POST");
+                            conn.setDoOutput(true);
+                            conn.setDoInput(true);
+                            conn.addRequestProperty("Authorization", "key=" + "AAAAWxrxiPs:APA91bHK5AQv5eBDtaJM2pf3U7LO9ApAstz-Zn0asmOPouh7CJyn053wf3RHfrWtQmyUQvblcakUOKpBVqyO-BBW1QrTp4mpp8eJEtaJ13NiOc3NOE6oZ6DQu2X9acQdMhNu730PODDb");
+                            conn.setRequestProperty("Accept", "application/json");
+                            conn.setRequestProperty("Content-type", "application/json");
+                            OutputStream os = conn.getOutputStream();
+                            os.write(root.toString().getBytes("utf-8"));
+                            os.flush();
+                            conn.getResponseCode();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+
+
+//                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+//                AdminNoticeFragment homeFragment = new AdminNoticeFragment();
+//                ListFragment listFragment = new ListFragment();
+//
+//                transaction.replace(R.id.frame_layout, listFragment).commitAllowingStateLoss();
             }
         });
 
